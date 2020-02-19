d99e7f03d3c576e472e71f4739ea3282a8e8c893
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2020-02-19 03:23:46 +0200

    Added multi-sound support

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,41 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
-    
-
+	
+	var players: [AVAudioPlayer] = []
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	@IBAction func keyPressed(_ sender: UIButton) {
+		sender.alpha = 0.5
+		playSound(filename: sender.currentTitle!)
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+			sender.alpha = 1
+		}
+	}
+	
+	func playSound(filename: String) {
+		let url = Bundle.main.url(forResource: filename, withExtension: "wav")
+		let player = try! AVAudioPlayer(contentsOf: url!)
+		players.append(player)
+		player.play()
+		
+		for pl in players {
+			if !pl.isPlaying {
+				if let index = players.firstIndex(of: pl) {
+					players.remove(at: index)
+				}
+			}
+		}
+		
+	}
+	
 }
+
 
